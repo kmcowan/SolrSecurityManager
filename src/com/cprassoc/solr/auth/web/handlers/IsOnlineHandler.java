d5b0c5f8cf6669bb57f7430a5961a850c0107e3c
kevin <kevin@192.168.0.102>
2018-04-06 19:53:07 -0600

    add isonline handler

---
+++ src/com/cprassoc/solr/auth/web/handlers/IsOnlineHandler.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cprassoc.solr.auth.web.handlers;
+
+import com.cprassoc.solr.auth.SolrAuthActionController;
+import com.cprassoc.solr.auth.util.Utils;
+import com.cprassoc.solr.auth.web.handlers.model.Handler;
+import com.cprassoc.solr.auth.web.html.HTML;
+import com.sun.net.httpserver.HttpExchange;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author kevin
+ */
+public class IsOnlineHandler implements Handler{
+    
+    public byte[] handle(JSONObject action, HttpExchange ex){
+       // byte[] bytes = null;
+        JSONObject obj = SolrAuthActionController.getServerStatus();
+      //  bytes = Utils.streamToBytes(HTML.class.getResourceAsStream("test.html"));
+        return obj.toString().getBytes();
+    }
+    
+}
