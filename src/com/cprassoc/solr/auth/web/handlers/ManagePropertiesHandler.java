1645fa3cbf6c50aadff6bc12037746784844a9e1
kevin <kevin@192.168.0.102>
2018-04-07 13:59:30 -0600

    add properties handler

---
+++ src/com/cprassoc/solr/auth/web/handlers/ManagePropertiesHandler.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cprassoc.solr.auth.web.handlers;
+
+import com.cprassoc.solr.auth.SolrAuthActionController;
+import com.cprassoc.solr.auth.model.SecurityJson;
+
+import com.cprassoc.solr.auth.web.handlers.model.Handler;
+
+import com.sun.net.httpserver.HttpExchange;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author kevin
+ */
+public class ManagePropertiesHandler implements Handler{
+     private SecurityJson securityJson = null;
+     
+    public byte[] handle(JSONObject action, HttpExchange ex){
+       JSONObject obj = new JSONObject();
+        securityJson = null;
+        SolrAuthActionController.SOLR.getAuthentication();
+        String currAction = action.getString("name");
+        switch(currAction){
+            case "listusers":
+                
+                break;
+        }
+      
+    
+        return obj.toString().getBytes();
+    }
+    
+}
